eb67ed9f9bf330068df1113897412ac58b325349
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-21 22:09:03 -0400

    all function tests passed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2);
+}
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, two) => {
+    if(two === returnFirstTwoDrivers) {
+        return drivers.slice(0, 2);
+    } else if(two === returnLastTwoDrivers) {
+        return drivers.slice(-2);
+    }
+}
